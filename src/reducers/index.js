8c428874be8c63ffdf2cb5bb87acf9af5351c1c1
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-13 22:09:30 +0000

    Automatically backed up by Learn

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,10 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state, action) {
+  switch (action.type) {
+    case ADD_INGREDIENT:
+      return()
+    case REMOVE_INGREDIENT:
+      return()
+    default:
+      return state
+  }
 }
