c763e9f5fc9895dea268a46804cb8cbb9f1523e7
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-13 22:17:17 +0000

    Automatically backed up by Learn

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 export default function ingredientReducer(state=[], action) {
   switch (action.type) {
-    case ADD_INGREDIENT:
+    case "ADD_INGREDIENT":
       return[
         ...state,
         action.payload
       ]
-    case REMOVE_INGREDIENT:
+    case "REMOVE_INGREDIENT":
       return(
         [...state].filter(function(value){
           return value !== action.payload ;
